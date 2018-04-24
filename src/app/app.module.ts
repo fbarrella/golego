e6a29448a35bd374c5d42bcf184f4aa7064bbe9f
Victor Santos <victorvieirads@gmail.com>
2018-04-24 14:49:18 -0300

    Adicionando dependencias do firebase e angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule, AngularFireAuth } from "angularfire2/auth";
+import { AngularFirestoreModule } from "angularfire2/firestore"
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
-import { CadastroPage } from '../pages/cadastro/cadastro';
+
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { LoginPage } from "../pages/login/login";
+import { CadastroPage } from "../pages/cadastro/cadastro";
+
+import { FIREBASE_CONFIG } from "./firebase.credentials";
 
 @NgModule({
   declarations: [
@@ -18,7 +24,10 @@ import { CadastroPage } from '../pages/cadastro/cadastro';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
