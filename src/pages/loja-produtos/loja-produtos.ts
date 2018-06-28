07c9fec019f52a15dd89db2f29e57fbb220ef13d
Victor <victorvieirads@gmail.com>
2018-06-27 22:21:58 -0300

    mockup da tela de produtos da loja

--- src/pages/loja-produtos/loja-produtos.ts
+++ src/pages/loja-produtos/loja-produtos.ts
@@ -8,11 +8,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LojaProdutosPage {
 
+  produtos = [
+    { nome: "Produto 01", id: "123456789", qt: 10 },
+    { nome: "Produto 02", id: "223456789", qt: 0 },
+    { nome: "Produto 03", id: "323456789", qt: 100 },
+    { nome: "Produto 04", id: "423456789", qt: 200 },
+    { nome: "Produto 05", id: "523456789", qt: 10 },
+    { nome: "Produto 06", id: "623456789", qt: 5 },
+    { nome: "Produto 07", id: "723456789", qt: 37 },
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LojaProdutosPage');
+
+  }
+
+  pushEditarProduto(produto) {
+    console.log(produto)
+  }
+
+  addProduto() {
+
   }
 
 }
